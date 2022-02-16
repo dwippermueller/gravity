d52bf26bc67d71d2cefea14dc8593fdb55222d06
Brian O'Reilly <boreilly@guidewire.com>
2022-02-16 14:59:02 +0000

    fix movement

--- src/gameScene.ts
+++ src/gameScene.ts
@@ -24,24 +24,24 @@ export class GameScene extends Phaser.Scene {
     public create() {
       this.starfield = this.add.tileSprite(window.innerWidth / 2, window.innerHeight / 2, window.innerWidth, window.innerHeight, 'starfield')
       this.player = this.physics.add.sprite(40, 40, 'player1')
-      this.player.body.setGravityY(100)
-      this.player.body.angle = 90
+      this.player.body.setGravityY(50)
     }
    
     public update() {
         this.starfield.tilePositionY += 2
         const cursorKeys = this.input.keyboard.createCursorKeys()
- 
         if (cursorKeys.space.isDown) {
-            const velocity = this.physics.velocityFromAngle(this.player.body.rotation, 10)
+            const velocity = this.physics.velocityFromAngle(this.player.body.rotation - 90, 5)
             this.player.body.velocity.x += velocity.x
             this.player.body.velocity.y += velocity.y
            }
  
         if (cursorKeys.right.isDown) {
-          this.player.body.angularVelocity += 2
+          this.player.body.angularVelocity = 60
         } else if (cursorKeys.left.isDown) {
-          this.player.body.angularVelocity -= 2
+          this.player.body.angularVelocity = -60
+        } else {
+            this.player.body.angularVelocity = 0
         }
         
     }
