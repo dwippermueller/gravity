a51991de56b251f44dfdabb89c759c37c3dbf00b
Dirk Wippermueller <dirk.wippermueller@gmail.com>
2022-02-17 09:00:11 +0100

    Added new starship

--- src/gameScene.ts
+++ src/gameScene.ts
@@ -21,7 +21,6 @@ export class GameScene extends Phaser.Scene {
     private player2: Phaser.Types.Physics.Arcade.GameObjectWithDynamicBody
     private starfield: Phaser.GameObjects.TileSprite
     private bullets: BulletGroup
-    private bulletTime: number = 0
 
     constructor() {
         super(sceneConfig);
@@ -29,10 +28,9 @@ export class GameScene extends Phaser.Scene {
 
     public preload() {
         this.load.image('starfield', 'assets/starfield.png')
-        this.load.image('player1', 'assets/player.png')
-        this.load.image('player2', 'assets/player2.png')
         this.load.image('bullet', 'assets/enemy-bullet.png')
-        this.load.spritesheet('starship', 'assets/starship-sheet.png', {frameWidth: 208, frameHeight: 324})
+        this.load.spritesheet('starship', 'assets/starship-sheet.png', { frameWidth: 208, frameHeight: 324 })
+        this.load.spritesheet('starship2', 'assets/starship-sheet2.png', { frameWidth: 208, frameHeight: 324 })
     }
 
     public create() {
@@ -41,25 +39,37 @@ export class GameScene extends Phaser.Scene {
         this.starfield = this.add.tileSprite(windowWidth / 2, windowHeight / 2, windowWidth, windowHeight, 'starfield')
 
         this.anims.create({
-            key: 'no-thrust',
+            key: 'no-thrust1',
             frames: this.anims.generateFrameNumbers('starship', { start: 0, end: 3 }),
             frameRate: 5,
             repeat: -1
         })
         this.anims.create({
-            key: 'thrust',
+            key: 'thrust1',
             frames: this.anims.generateFrameNumbers('starship', { start: 4, end: 7 }),
             frameRate: 5,
             repeat: -1
         })
-        
+        this.anims.create({
+            key: 'no-thrust2',
+            frames: this.anims.generateFrameNumbers('starship2', { start: 0, end: 3 }),
+            frameRate: 5,
+            repeat: -1
+        })
+        this.anims.create({
+            key: 'thrust2',
+            frames: this.anims.generateFrameNumbers('starship2', { start: 4, end: 7 }),
+            frameRate: 5,
+            repeat: -1
+        })
+
         this.player1 = this.createPlayer('starship', 40, 40, 0.2)
-        this.player2 = this.createPlayer('starship', 400, 400, 0.2)
+        this.player2 = this.createPlayer('starship2', 400, 400, 0.2)
 
         this.bullets = new BulletGroup(this)
     }
 
-    public createPlayer(sprite : string, xPos : integer, yPos : integer, scale = 1) {
+    public createPlayer(sprite: string, xPos: integer, yPos: integer, scale = 1) {
         let player = this.physics.add.sprite(xPos, yPos, sprite)
         player.scale = scale
         player.body.setGravityY(70)
@@ -75,31 +85,29 @@ export class GameScene extends Phaser.Scene {
         const aKey = this.input.keyboard.addKey('A')
         const dKey = this.input.keyboard.addKey('D')
 
-        this.movePlayer(this.player1, cursorKeys.up, cursorKeys.space, cursorKeys.left, cursorKeys.right)
-        this.movePlayer(this.player2, wKey, cursorKeys.shift, aKey, dKey)
+        this.movePlayer(this.player1, cursorKeys.up, cursorKeys.space, cursorKeys.left, cursorKeys.right, 1)
+        this.movePlayer(this.player2, wKey, cursorKeys.shift, aKey, dKey, 2)
     }
 
     public movePlayer(
-        player : GameObjectWithDynamicBody,
-        thrust : Phaser.Input.Keyboard.Key,
-        fire : Phaser.Input.Keyboard.Key,
-        left : Phaser.Input.Keyboard.Key,
-        right : Phaser.Input.Keyboard.Key
+        player: GameObjectWithDynamicBody,
+        thrust: Phaser.Input.Keyboard.Key,
+        fire: Phaser.Input.Keyboard.Key,
+        left: Phaser.Input.Keyboard.Key,
+        right: Phaser.Input.Keyboard.Key,
+        playerNumber: integer
     ) {
         if (thrust.isDown) {
-            (player as Phaser.GameObjects.Sprite).anims.play('thrust', true)
+            (player as Phaser.GameObjects.Sprite).anims.play('thrust' + playerNumber, true)
             const velocity = this.physics.velocityFromAngle(player.body.rotation - 90, 10)
             player.body.velocity.x += velocity.x
             player.body.velocity.y += velocity.y
         } else {
-            (player as Phaser.GameObjects.Sprite).anims.play('no-thrust', true)
+            (player as Phaser.GameObjects.Sprite).anims.play('no-thrust' + playerNumber, true)
         }
-        if (fire.isDown) {
-            if (this.game.getTime() > this.bulletTime) {
-                const velocity = this.physics.velocityFromAngle(player.body.rotation - 90, 500)
-                this.bullets.fireBullet(player.body.x, player.body.y, velocity)
-                this.bulletTime = this.game.getTime() + 150
-            }
+        if (Phaser.Input.Keyboard.JustDown(fire)) {
+            const velocity = this.physics.velocityFromAngle(player.body.rotation - 90, 500)
+            this.bullets.fireBullet(player.body.x + 15, player.body.y + 20, velocity.add(player.body.velocity))
         }
 
         if (right.isDown) {
